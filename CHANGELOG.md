0ea8106ffa5baa0bf958ca8df28bdb10b0542469
Julian Stier <mail@julian-stier.de>
2019-03-03 09:16:00 +0100

    Version and changelog added

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 0.2
+- added a population class which allows operations over sets of gene codes (concrete instances, given a gene code space description)
+- added VERSION and CHANGELOG.md files
+- fixed test errors on fit() invocations
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import os
 from setuptools import setup
+
+with open(os.path.join('./', 'VERSION')) as version_file:
+    project_version = version_file.read().strip()
 
 setup(
     name='kayak',
-    version='0.0.2',
+    version=project_version,
     description='Genetic Encoding Space Description Framework',
     author='Chair of Data Science',
     author_email='julian.stier@uni-passau.de',
