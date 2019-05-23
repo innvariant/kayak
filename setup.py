6fd0b7454a450e553b22cf020ee934bc3c8df9d1
Julian Stier <mail@julian-stier.de>
2019-05-23 11:55:29 +0200

    Requirements constructed from environment file

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import yaml
 from setuptools import setup
+
+environment = yaml.load('environment.yml')
+environment_requirements = list()
+for elem in environment['dependencies']:
+    if isinstance(elem, str):
+        # TODO: convert package=0.0 to package==0.0
+        environment_requirements.append(elem)
+    elif isinstance(elem, dict) and 'pip' in elem:
+        environment_requirements.extend(elem['pip'])
+print('\n'.join(environment_requirements))
 
 setup(
     name='kayak',
@@ -8,7 +19,7 @@ setup(
     author_email='julian.stier@uni-passau.de',
     packages=['kayak'],
     py_modules=['kayak'],
-    install_requires=[],
+    install_requires=environment_requirements,
     dependency_links=[],
     extras_require={
         'NetworkX': ['networkx']
